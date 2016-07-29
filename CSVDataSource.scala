78e53e27854992b898b4f6fb84aa9bb8d0a9414d
HyperPiGuy <mattlikespi@gmail.com>
2016-07-29 19:03:09 -0400

    Add files via upload

---
+++ CSVDataSource.scala
@@ -0,0 +1,89 @@
+package toolkit.neuralnetwork.source
+
+import libcog._
+import toolkit.neuralnetwork.DifferentiableField
+import toolkit.neuralnetwork.policy.StandardLearningRule
+import toolkit.neuralnetwork.layer.BiasLayer
+
+/**
+* The CSVDataSource will read in data from a CSV file and populate a field
+* with the data with each element being the Shape of elemSize
+*
+*@param fileName CSV file to load
+*@param batchSize Examples to read from file
+*@param header True if csv file has a header row on top of file, false if not
+*@param areNums True if all data in file are numbers, False if data includes strings\characters
+*@param elemSize Shape of data to be loaded into layer
+*/
+
+case class CSVDataSource(fileName:String,
+                         override val batchSize:Int,
+                         header:Boolean, 
+                         areNums:Boolean,
+                         elemSize:Shape) extends DifferentiableField {
+
+    override val forward: Field = {
+
+      ////////////////////////////////read in csv data to Array
+      val bufferedSource = io.Source.fromFile(fileName)
+      val dataIn: Array[Array[String]] = {
+        var mat: Array[Array[String]] = Array.empty
+        val takeElem: Int =  header match{
+          case true => batchSize+1
+          case false => batchSize
+          }
+        var skipHeader = header
+        for (line <- bufferedSource.getLines.take(takeElem)) {
+          if (skipHeader) {
+            skipHeader = false
+          }
+          else {
+            val cols = line.split(",").map(_.trim)
+            mat = mat :+ cols
+          }
+        }
+        mat
+      }
+      bufferedSource.close
+
+      require(dataIn.length>=batchSize,s"${dataIn.length} examples found in file. This is not enough for the $batchSize batchSize")
+
+      val dataSet = dataIn.dropRight(dataIn.length-batchSize)
+      val Dsize = elemSize.toArray
+
+      require(dataSet(0).length==Dsize(0)*Dsize(1),s"The number of data points per example in file does not equal ${Dsize(0)*Dsize(1)}")
+
+      ////////////////Covert data to 2D Float Array
+      var Fmat: Array[Array[Float]] = Array.empty
+      if (areNums) {
+        Fmat = dataSet.map(_.map(x => x.toFloat))
+      } else {
+        Fmat = dataSet.map(_.map(x => x.toList.head.toByte.toFloat))
+      }
+      val dataArr = Fmat.transpose
+
+      //////////////put data into 2D Vector Array
+      val dataVecArr: Array[Array[Vector]] = {
+        var mat: Array[Array[Vector]] = Array.empty
+        var vArr: Array[Vector] = Array.empty
+        for (i <- 0 until Dsize(0)) {
+          vArr = Array.empty
+          for (j <- 0 until Dsize(1)) {
+            vArr = vArr :+ new Vector(dataArr(i*Dsize(1)+j))
+          }
+          mat = mat :+ vArr
+        }
+        mat
+      }
+
+      val VField = new VectorField(Dsize(0),Dsize(1), (i, j) => dataVecArr(i)(j))
+      val DF = DifferentiableField(VField, batchSize)
+
+      val fieldOut = {
+        if (areNums) {
+          DF.forward
+         }else {DF.forward / 255f}
+        }
+      fieldOut
+    }
+}
---
+++ CSVLabelSource.scala
@@ -0,0 +1,22 @@
+package toolkit.neuralnetwork.source
+
+import libcog._
+import toolkit.neuralnetwork.DifferentiableField
+import toolkit.neuralnetwork.operator.indexToOneHotCode
+
+/**
+  * This is the label version of the CSV source
+  * Place all of the labels into one single column
+  *
+  * @param fileName CSV file to load
+  * @param batchSize Number of examples to read from file
+  * @param classes Number of classifications
+  * @param header true if there is a header row at top of file, false otherwise
+  */
+case class CSVLabelSource (fileName:String,
+                           override val batchSize:Int,
+                           classes:Int,
+                           header:Boolean) extends DifferentiableField {
+  override val forward: Field =
+    DifferentiableField(indexToOneHotCode(CSVDataSource(fileName,batchSize,header,true,Shape(1,1)).forward,classes),batchSize).forward.toVectorField(0).toVectorField(0)
+}
